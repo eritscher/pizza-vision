762b73c6dd8ed05d490d7978d125f83b150e6970
Evan Ritscher <evan.ritscher@gmail.com>
2018-06-23 17:04:11 -0400

    standardize messaging

--- analyze_image.py
+++ analyze_image.py
@@ -1,10 +1,10 @@
 import boto3
-from os import environ
+from os import getenv
 
 
 def analyze_image(image):
-    ACCESS_KEY = environ.get('AWS_ACCESS_KEY')
-    SECRET = environ.get('AWS_SECRET_ACCESS_KEY')
+    ACCESS_KEY = getenv('AWS_ACCESS_KEY')
+    SECRET = getenv('AWS_SECRET_ACCESS_KEY')
     client = boto3.client('rekognition', aws_access_key_id=ACCESS_KEY,
                           aws_secret_access_key=SECRET)
     response = client.detect_labels(Image={'Bytes': image.read()})
--- api.py
+++ api.py
@@ -1,14 +1,25 @@
+from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from analyze_image import analyze_image
+load_dotenv()
 app = Flask(__name__)
 
 
-@app.route('/post', methods=['POST'])
-def post():
+@app.route('/pizza', methods=['POST'])
+def pizza():
     image = request.files['file']
     results = analyze_image(image)
-
-    return jsonify(results), 200
+    for el in results:
+        if el['Name'] == 'Pizza':
+            response = {
+                'message': 'Pizza confirmed.',
+                'confidence': el
+            }
+            return jsonify(response), 200
+    response = {
+        'message': 'Pizza not found.',
+    }
+    return jsonify(response), 200
 
 
 if __name__ == '__main__':
